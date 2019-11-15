32d6af8a182cff5dde9888bf5230ae22cb61445f
Jeewon Bae <jeewon.b@lgcns.com>
2019-11-15 12:38:23 +0900

    change folder name

--- IIOT_Components/src/components/auisoft/AUIGrid/AUIGridLicense.js
+++
@@ -1,9 +0,0 @@
-/*!
- * AUIGrid 3.0.7 License
- * License Type : Evaluation(Trial) Version
- * Authorized Domain(or IP) : localhost,127.0.0.1
- * Expiration Date : 2019-12-14
- * http://www.auisoft.net
- */
- /* eslint-disable */
-AUIGridLicense = "eyJjdCI6Ik9JcHZyakJvWTVIRG5jVVhzUENQRUpRZWFTeHR6STMwbGVmZmYxZXRmQUk4dDVqWHhsRmN1NTQyY01JZURJM0Z5SHBuYm45VUdENXJGS3BRbk9OTkVnaldPa3p5TVJ5MlJcL2hLNEVtOHpjRT0iLCJpdiI6ImFhMDE3M2YyZTg2OWJlMmM3YTUxYTFjZGE5MzZjNzYwIiwicyI6IjViYWQwNjJkYzBmYThmN2UifQ==";
